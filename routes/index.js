34609fef24bb6de327ef65d64190c0fdfeffbcf3
crossproject <dnnyparra@gmail.com>
2024-05-21 19:37:37 -0300

    Added eslint and prettier

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.get("/", (req, res) => {
-  res.send("Welcome to Final Project Team 17");
+router.get('/', (req, res) => {
+  res.send('Welcome to Final Project Team 17');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 // Routes
-app.use("/", require("./routes/"));
+app.use('/', require('./routes/'));
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
